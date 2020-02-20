d090254f5f1f4b468f70541c4472aec1d8f940ec
Neon <njuarez@hermescorp.io>
2020-02-19 18:06:43 -0600

    express

---
+++ route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3002;
+const cors = require('cors');
+const path = require('path');
+
+app.use(cors());
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './', 'index.html')) );
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
