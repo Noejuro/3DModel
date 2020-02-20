e0483c30c03fb3f5e962e87c675b585da88bb428
Neon <njuarez@hermescorp.io>
2020-02-20 15:41:34 -0600

    public_added

--- route.js
+++ route.js
@@ -6,6 +6,6 @@ const path = require('path');
 
 app.use(cors());
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './', 'index.html')) );
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public', 'index.html')) );
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
